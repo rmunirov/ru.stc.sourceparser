9e23c4334a48e2bd7763da29086b178489ce5d7c
rmunirov <r.munirov.stc@innopolis.university>
2018-10-02 13:48:22 +0500

    new branch, added tests for class ResultWriter

---
+++ src/test/java/ru/innopolis/stc12/sourceparser/ResultWriterTest.java
@@ -0,0 +1,19 @@
+package ru.innopolis.stc12.sourceparser;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ResultWriterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setFinish() {
+    }
+
+    @Test
+    void run() {
+    }
+}
