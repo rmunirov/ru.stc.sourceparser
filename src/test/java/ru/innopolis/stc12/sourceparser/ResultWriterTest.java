350e371022b788be6c000e58a2e8c041316296c7
rmunirov <r.munirov.stc@innopolis.university>
2018-10-02 13:55:39 +0500

    fix bug

--- src/test/java/ru/innopolis/stc12/sourceparser/ResultWriterTest.java
+++ src/test/java/ru/innopolis/stc12/sourceparser/ResultWriterTest.java
@@ -7,13 +7,16 @@ class ResultWriterTest {
 
     @BeforeEach
     void setUp() {
+        // because i don't know how need are write test for threads
     }
 
     @Test
     void setFinish() {
+        // because i don't know how need are write test for threads
     }
 
     @Test
     void run() {
+        // because i don't know how need are write test for threads
     }
 }
